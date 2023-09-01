541498d1dfcb9be9782463fe5fc0044e0e11b3a1
Siborbor <89548886+Siborbor@users.noreply.github.com>
2023-09-01 17:06:31 -0500

    general styles

--- src/routers/Layout.jsx
+++ src/routers/Layout.jsx
@@ -1,16 +1,38 @@
+import { useEffect, useState } from "react";
 import LogoAmoba from "../assets/img/logo-amoba.png";
 import UserAvatar from "../assets/img/usuario.png";
 import IconMenuTop from "../assets/img/icon-menu.png";
 import MenuAcordeon from "../components/MenuAcordeon";
 import itemsMenu from "../data/itemsMenu";
 import { Outlet } from "react-router-dom";
+import { loginRequest, userProfileRequest } from "../api/autenticacion";
+import { useAuthStore, userProfileStore } from "../store/auth";
 
 const Layout = () => {
-    return (
+  const setToken = useAuthStore((state) => state.setToken);
+  const setUserData = userProfileStore((state) => state.setUserData);
+  const userdata = userProfileStore.getState().userProfileData;
+  
+  useEffect(() => {
+    (async () => {
+      try {
+        const restLogin = await loginRequest("test", "123456");
+        setToken(restLogin.access_token);
+        const restUserProfile = await userProfileRequest();
+        setUserData(restUserProfile);
+      } catch (error) {
+        console.error("Error en la solicitud de inicio de sesión:", error);
+      }
+    })();
+  }, []);
+  
+  return (
     <>
       <section className="topBar">
         <img src={LogoAmoba} alt="logo-amoba" />
-        <img src={UserAvatar} alt="avatar de usuario" />
+        <div>
+          <img src={UserAvatar} alt="avatar de usuario" />
+        </div>
       </section>
       <section className="layout">
         <div id="sidebar">
@@ -19,11 +41,11 @@ const Layout = () => {
               <img src={IconMenuTop} alt="icono del menu top" />
               <p>Menu Top</p>
             </div>
-            <MenuAcordeon itemsMenu={itemsMenu}/>
+            <MenuAcordeon itemsMenu={itemsMenu} />
           </nav>
         </div>
         <div id="detail">
-          <Outlet/>
+          <Outlet />
         </div>
       </section>
     </>
